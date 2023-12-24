528be0356ba3c248dbc95cf796171c55842a744d
cipgen <150297185+cipgen@users.noreply.github.com>
2023-12-24 00:23:24 +0000

    add jenkins groovy

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,18 +1,58 @@
 pipeline {
-    agent any
+agent any
+
+    environment {
+        REPO = "https://github.com/cipgen/kbot"
+        BRANCH = "main"
+    }
+
     parameters {
 
         choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
+        choice(name: 'ARCH', choices: ['amd64', 'arm64', 'all'], description: 'Pick ARCH')
 
     }
-    stages {
-        stage('Example') {
-            steps {
-                echo "Build for platform ${params.OS}"
 
-                echo "Build for arch: ${params.ARCH}"
+    stages {
+
+        stage('clone') {
+            steps {
+                echo 'CLONE REPOSITORY'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+        }
+
+        stage('test') {
+            steps {
+                echo 'TEST EXECUTION STARTED'
+                sh 'make test'
+            }
+        }
+
+        stage('build') {
+            steps {
+                echo 'BUILD EXECUTION STARTED'
+                sh 'make build'
+            }
+        }
+
+        stage('image') {
+            steps {
+                echo 'IMAGE EXECUTION STARTED'
+                sh 'make image'
+            }
+        }
+
+        stage('push') {
+            steps {
+                script {
+                    docker.withRegistry( '', 'dockerhub' ) {
+                    sh 'make push'
+                    }
+                }
 
             }
         }
+
     }
 }
